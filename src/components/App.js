5474524b97842712aff1a41eaad0c3a7eedd1d4b
ShivankTyagi <tyagishivank1212@gmail.com>
2023-02-17 18:45:10 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
   }
   useEffect(() => {
-    console.log(`Input in #text-input ${name}`)
+    console.log(`Input in #text-input is ${name}`)
   }, [name])
   useEffect(() => {
     console.log(number)
